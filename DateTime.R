af7a55342c8fc6ea82da34b661463c0d4303735f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-28 12:46:50 +0500

    Intro to date time using lubridate libarary

--- DateTime.R
+++ DateTime.R
@@ -3,3 +3,19 @@
 # loading lubridate =>  library(lubridate)
 
 # Dates and Times in R
+
+today() #check current date
+
+now() #check current date time
+
+ymd("2021-01-20")
+
+mdy("January 20th, 2021")
+
+dmy("20-Jan-2021")
+
+ymd(20210120)
+
+#switch between date-time to date
+
+as_date(now()) 
