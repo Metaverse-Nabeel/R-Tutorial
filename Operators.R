186b766ade55bc63dddefd97cc285d3b1fdd1e6b
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-29 11:27:56 +0500

    Operators in R

---
+++ Operators.R
@@ -0,0 +1,12 @@
+# our first calculations (run the code)
+
+quarter_1_sales <- 35657.98 
+
+quarter_2_sales <- 43810.55 
+
+midyear_sales <- quarter_1_sales + quarter_2_sales
+
+yearly_sales <- midyear_sales * 2
+
+print('Yearly sales = ')
+print(yearly_sales)
