02125bd190f667ce9df9fa98c91b82033be864a4
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-29 11:53:25 +0500

    Lesson 3 activity complete

---
+++ lesson_3_R_sandbox_activity.R
@@ -0,0 +1,37 @@
+#install the tidyverse package
+install.packages("tidyverse")
+
+#load the tidyverse package
+library(tidyverse)
+
+#previewing data in diamonds data-set
+
+head(diamonds)
+
+#these functions can also be used to get the glimpse of the data-set
+
+str(diamonds)
+
+glimpse(diamonds)
+
+colnames(diamonds)
+
+#renaming the column names
+
+rename(diamonds, carat_new = carat, cut_new = cut)
+
+#summarizing data
+
+summarize(diamonds, mean_carat = mean(carat))
+
+#visualizing the data
+
+ggplot(data = diamonds, aes(x = carat, y = price)) +
+  geom_point()
+
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +
+  geom_point()
+
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +
+  geom_point() +
+  facet_wrap(~cut)
