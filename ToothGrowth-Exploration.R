e71c164106127962cec3e4e8a52f7ddae24cce7e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-30 11:56:00 +0500

    explore toothgrowth data with filters

---
+++ ToothGrowth-Exploration.R
@@ -0,0 +1,12 @@
+#Load ToothGrowth data already available in R
+
+data("ToothGrowth")
+
+View(ToothGrowth)
+
+filtered_tg <- filter(ToothGrowth, dose == 0.5)
+View(filtered_tg)
+
+arrange(filtered_tg, len)
+
+#Making use of Nested Functions
