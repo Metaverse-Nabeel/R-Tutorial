e70bc777718d586f1139bf0c0593a2231ab48d10
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-01-01 11:46:03 +0500

    Organize and filter palmerpenguins data

--- cleaning_data.R
+++ cleaning_data.R
@@ -37,3 +37,16 @@ penguins %>% arrange(-bill_length_mm)
 
 penguins_desc <- penguins %>% arrange(-bill_length_mm)
 View(penguins_desc)
+
+# To get the mean bill lengths of Island
+penguins %>% group_by(island) %>% drop_na() %>% summarise(mean_bill_length_mm = mean(bill_length_mm))
+
+# To get the max bill lengths of Island
+penguins %>% group_by(island) %>% drop_na() %>% summarise(max_bill_length_mm = max(bill_length_mm))
+
+# To get the max and mean bill lengths of Island and Species
+penguins %>% group_by(species, island) %>% drop_na() %>% summarise(max_bl = max(bill_length_mm), mean_bl = mean(bill_length_mm))
+
+# To filter using species
+penguins %>% filter(species == "Adelie")
+
