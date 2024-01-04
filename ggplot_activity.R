50be1b14014c5e1581ad79f2f0ed60cd94de31e7
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-01-04 17:11:32 +0500

    ggplot activity complete

---
+++ ggplot_activity.R
@@ -0,0 +1,17 @@
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+data("penguins")
+View(penguins)
+
+#ggplot to create scatter plot using the given statement
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+
+#ggplot to create scatter plot using for hotels and lead_time
+View(hotel_bookings)
+
+ggplot(data = hotel_bookings) + geom_point(mapping = aes(x = hotel, y = lead_time))
