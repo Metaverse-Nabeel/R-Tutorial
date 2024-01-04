b68e909782cb8963e9a6b6aa5b40c59af8b74fd9
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-01-04 18:44:04 +0500

    geom function

--- ggplot_activity.R
+++ ggplot_activity.R
@@ -8,10 +8,20 @@ View(penguins)
 
 #ggplot to create scatter plot using the given statement
 
-ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+ggplot(data = penguins) +
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
 
 
 #ggplot to create scatter plot using for hotels and lead_time
 View(hotel_bookings)
 
-ggplot(data = hotel_bookings) + geom_point(mapping = aes(x = hotel, y = lead_time))
+ggplot(data = hotel_bookings) +
+  geom_point(mapping = aes(x = hotel, y = lead_time))
+
+library(tidyverse)
+
+#ggplot to create scatter plot
+ggplot(data = penguins) +
+  geom_point(mapping = aes(x = bill_depth_mm, y = bill_length_mm))
+ 
+
