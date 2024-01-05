3269eac8499cfaa2ca1f29da36b7f3ef9a2c616b
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-01-05 17:08:02 +0500

    face_grid with datasets

--- facets_aesthetics.R
+++ facets_aesthetics.R
@@ -13,3 +13,10 @@ ggplot(data = penguins, aes(x=flipper_length_mm, y=body_mass_g))+
 ggplot(data = diamonds)+
   geom_bar(mapping=aes(x=color, fill=cut))+
   facet_wrap(~cut)
+
+
+#facet_grid
+
+ggplot(data = penguins)+
+  geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g, color=species))+
+  facet_grid(sex~species)
