eb9c7c88515da262500ce79a776315e98257a7a0
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-01-06 18:36:05 +0500

    Annotation layer

--- facets_aesthetics.R
+++ facets_aesthetics.R
@@ -20,3 +20,10 @@ ggplot(data = diamonds)+
 ggplot(data = penguins)+
   geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g, color=species))+
   facet_grid(sex~species)
+
+#annotations
+
+ggplot(data = penguins)+
+  geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g, color=species))+
+  labs(title = "Palmer Penguins: Testing scatter plot title", subtitle = "Check the subtitle as well", caption = "Engr. Nabeel Ahmed")+
+  annotate("text", x = 220, y=3500, label="Check annotate", color="purple", fontface="bold", size=4.5, angle=25)
